20d83b9c7bed459f3c9d676cfca00c4c35ab97ea
Franck Fernandez <franck.fernandez@gmail.com>
2019-12-29 21:58:34 +0100

    Comment all Image component

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-const Nav = ({}) => (
+const Nav = (props) => (
   <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div className="container">
       <a className="navbar-brand js-scroll-trigger" href="#page-top">
