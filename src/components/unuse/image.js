16bd37fd455a5f384c62706ee262cb53a9bf5afe
Franck Fernandez <franck.fernandez@gmail.com>
2019-12-29 22:33:39 +0100

    Comment unused files

--- src/components/unuse/image.js
+++ src/components/unuse/image.js
@@ -13,7 +13,7 @@
  * - `useStaticQuery`: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-const Image = () => {
+// const Image = () => {
   // const data = useStaticQuery(graphql`
   //   query {
   //     placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
@@ -27,6 +27,6 @@ const Image = () => {
   // `)
 
   // return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
-}
+// }
 
-export default Image
+// export default Image
