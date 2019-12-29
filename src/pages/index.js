be2c56f32627bebe32acfe93b3a6c6e479440676
Franck Fernandez <franck.fernandez@gmail.com>
2019-12-29 18:25:01 +0100

    Comment all default code + import main, bootstrap css + import Nav component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,35 @@
 import React from "react"
+import Nav from "../components/Nav"
+import '../components/bootstrap.min.css'
+import '../components/agency.css'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+// import Layout from "../components/layout"
+// import Image from "../components/image"
+// import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-  </Layout>
+  <>
+    <Nav />
+    {/* Nav
+    Header
+    Section
+    Section
+    Section
+    Section
+    Footer */}
+  </>
 )
+
+// const IndexPage = () => (
+//   <Layout>
+//     <SEO title="Home" />
+//     <h1>Hi people</h1>
+//     <p>Welcome to your new Gatsby site.</p>
+//     <p>Now go build something great.</p>
+//     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+//       <Image />
+//     </div>
+//   </Layout>
+// )
 
 export default IndexPage
