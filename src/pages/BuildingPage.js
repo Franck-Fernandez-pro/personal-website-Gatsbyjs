8bfff86928250f54373b7289b4271a9403fbbfbf
Franck Fernandez <franck.fernandez@gmail.com>
2020-01-02 13:57:02 +0100

    Page to hide site while dev

---
+++ src/pages/BuildingPage.js
@@ -0,0 +1,29 @@
+import React from "react"
+
+const BuildingPage = () => (
+  <div
+    style={{
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      width: "100%",
+      minHeight: "100vh",
+      background: "linear-gradient(-45deg, #ac32e4, #7918f2, #4801ff)",
+    }}
+  >
+    <h3
+      style={{
+        fontFamily: "Montserrat - Black",
+        fontSize: "50px",
+        color: "#fff",
+        lineHeight: "1.2",
+        textTransform: "uppercase",
+        textAlign: "center",
+      }}
+    >
+      Site en cours de construction...
+    </h3>
+  </div>
+)
+
+export default BuildingPage
