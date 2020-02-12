5ea907daa220c8913f9f4cde73787f63f8788221
Franck Fernandez <franck.fernandez@gmail.com>
2020-02-12 14:17:10 +0100

    Improve Nav onScroll

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,36 +1,60 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import classNames from "classnames"
 
-// "navbar-shrink" HARD CODED --> only on scroll
-const Nav = ({ headerBtnLeft, headerBtns }) => (
-  <nav className="navbar navbar-expand-lg navbar-light fixed-top navbar-shrink" id="mainNav">
-    <div className="container">
-      <a className="navbar-brand js-scroll-trigger" href="#page-top">
-        {headerBtnLeft}
-      </a>
-      <button
-        className="navbar-toggler navbar-toggler-right"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarResponsive"
-        aria-controls="navbarResponsive"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <i className="fa fa-bars"></i>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarResponsive">
-        <ul className="navbar-nav text-uppercase ml-auto">
-          {headerBtns.map(({ text, href }, key) => (
-            <li key={key} className="nav-item">
-              <a className="nav-link js-scroll-trigger" href={href}>
-                {text}
-              </a>
-            </li>
-          ))}
-        </ul>
+const Nav = ({ headerBtnLeft, headerBtns }) => {
+  const [shrink, setShrink] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll)
+    return () => window.removeEventListener("scroll", handleScroll)
+  })
+
+  function handleScroll(e) {
+    if (window.scrollY && window.scrollY >= 120 && !shrink) {
+      setShrink(true)
+    }
+
+    if (window.scrollY && window.scrollY < 120 && shrink) {
+      setShrink(false)
+    }
+  }
+
+  return (
+    <nav
+      className={classNames("navbar navbar-expand-lg navbar-light fixed-top", {
+        "navbar-shrink": shrink,
+      })}
+      id="mainNav"
+    >
+      <div className="container">
+        <a className="navbar-brand js-scroll-trigger" href="#page-top">
+          {headerBtnLeft}
+        </a>
+        <button
+          className="navbar-toggler navbar-toggler-right"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarResponsive"
+          aria-controls="navbarResponsive"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <i className="fa fa-bars"></i>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarResponsive">
+          <ul className="navbar-nav text-uppercase ml-auto">
+            {headerBtns.map(({ text, href }, key) => (
+              <li key={key} className="nav-item">
+                <a className="nav-link js-scroll-trigger" href={href}>
+                  {text}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
-)
+    </nav>
+  )
+}
 
 export default Nav
