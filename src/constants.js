83f3fc5b44987263431d1f7e5de85917fa5f09dd
Franck Fernandez <36079133+Franck-Fernandez-pro@users.noreply.github.com>
2020-03-01 00:47:39 +0100

    FIX Content

--- src/constants.js
+++ src/constants.js
@@ -274,7 +274,7 @@ const websiteContent = {
         {
           title: "Informatique",
           content:
-            "Passionné depuis toujours, j'ai toujours été attiré a tout ce qui touche l'informatique et les nouvelles technologi.",
+            "Passionné depuis toujours, j'ai toujours été attiré a tout ce qui touche l'informatique et les nouvelles technologie.",
           icon: "fa-desktop",
           iconColor: "text-primary",
         },
