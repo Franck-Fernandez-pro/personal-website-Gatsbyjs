d5700a27d9cc0d97c3a8d9b740d251c73a8fe7c6
Franck Fernandez <franck.fernandez@gmail.com>
2020-07-15 18:20:52 +0200

    Run prettier

--- src/components/HeaderSection.jsx
+++ src/components/HeaderSection.jsx
@@ -1,22 +1,24 @@
 import React from "react"
 
-const HeaderSection = ({ content: { title, content, btnContent } }) => (
-  <header className="masthead">
-    <div className="container">
-      <div className="intro-text">
-        <div className="intro-lead-in">
-          <h1>{title}</h1>
+const HeaderSection = ({ content: { title, content, btnContent } }) => {
+  return (
+    <header className="masthead">
+      <div className="container">
+        <div className="intro-text">
+          <div className="intro-lead-in">
+            <h1>{title}</h1>
+          </div>
+          <div className="intro-heading text-uppercase">{content}</div>
+          <a
+            className="btn btn-primary btn-xl text-uppercase js-scroll-trigger"
+            href="#atouts"
+          >
+            {btnContent}
+          </a>
         </div>
-        <div className="intro-heading text-uppercase">{content}</div>
-        <a
-          className="btn btn-primary btn-xl text-uppercase js-scroll-trigger"
-          href="#atouts"
-        >
-          {btnContent}
-        </a>
       </div>
-    </div>
-  </header>
-)
+    </header>
+  )
+}
 
 export default HeaderSection
